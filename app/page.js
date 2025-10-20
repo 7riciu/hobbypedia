9640f2f3d38f774acbcf5ce2b5c280034017d315
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-10-20 22:35:04 +0300

    Added my first css element

---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Hello from "../components/MyComponent";
+
+function App() {
+  return (
+    <div>
+      This is my components
+      <Hello />
+    </div>
+  );
+}
+
+export default App;
---
+++ components/MyComponent.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Style from "../app/style.module.css";
+
+function Hello() {
+    return (
+        <div className={Style.container}>
+            <h1>Hello, world ^^</h1>
+        </div>
+    );
+}
+
+export default Hello;
