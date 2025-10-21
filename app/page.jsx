d568055a477c59a0cbfa0b4a620204a7bde72c69
Tepordei Patricia <tepordeipatricia2007@gmail.com>
2025-10-21 21:42:25 +0300

    I learned about conditional rendering

--- app/page.jsx
+++ app/page.jsx
@@ -2,14 +2,20 @@ import Hello from "../components/MyComponent";
 import Hobbies from "../components/Hobbies";
 
 function App() {
+const hobbyNumber = 1;
+
   return (
     <div>
-      <p>This is my components</p>
-      <Hello display = "Im Patricia"/>
-      <Hello display = "I love art"/>
-      <Hobbies hobby = {{title: "coding", time: "always"}}/>
-    </div>
-  )
+      {hobbyNumber === 1 ? (
+        <>
+          <p>This is my components</p>
+          <Hello display = "Im Patricia"/>
+          <Hello display = "I love art"/>
+          <Hobbies hobby = {{title: "coding", time: "always"}}/>
+        </>
+      ) : null }
+      </div>
+  );
 }
 
-export default App
+export default App;
